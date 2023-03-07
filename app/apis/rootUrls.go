e3e6ebd862a868e60c006f4e71cc751077e9f566
PatrickStar <1592810043@qq.com>
2023-03-07 10:18:52 +0800

    添加了正方 url 的自检测功能

--- app/apis/rootUrls.go
+++ app/apis/rootUrls.go
@@ -3,7 +3,7 @@ package apis
 import "os"
 
 var LIBRARY_URL = os.Getenv("LIBRARY_URL")
-var CAPTCHA_BREAKER_URL = os.Getenv("CAPTCHA_BREAKER_URL")
+var CAPTCHA_BREAKER_URL = os.Getenv("CAPTCHA_BREAKER_BK_URL")
 var CAPTCHA_NEW_BREAKER_URL = os.Getenv("CAPTCHA_BREAKER_NEW_URL")
 var ZF_URL = os.Getenv("ZF_URL")
 
