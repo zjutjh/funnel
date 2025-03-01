66925375dbadbf425c27f5ed577a2be2da28a2ae
MangoGovo <1749100231>
2025-03-02 02:01:33 +0800

    fix: 补充字段相关注释

--- app/model/scoreInfo.go
+++ app/model/scoreInfo.go
@@ -5,20 +5,20 @@ func TransformScoreInfo(raw *ScoreRawInfo) ScoreInfo {
 	for _, value := range raw.Items {
 		scoreInfo = append(scoreInfo,
 			&Score{
-				Score:       value.Cj,
-				ScorePoint:  value.Jd,
-				LessonName:  value.Kcmc,
-				LessonID:    value.Kch,
-				TeacherName: value.Jsxm,
-				ClassName:   value.Jxbmc,
-				Credits:     value.Xf,
-				SubmitTime:  value.Tjsj,
-				SubmitName:  value.Tjrxm,
-				LessonType:  value.Kcxzmc,
-				ExamType:    value.Ksxz,
-				SchoolTerm:  value.Xqmmc,
-				SchoolYear:  value.Xnmmc,
-				Key:         value.Key,
+				Score:       value.Cj,     // 成绩
+				ScorePoint:  value.Jd,     // 绩点
+				LessonName:  value.Kcmc,   // 课程名称
+				LessonID:    value.Kch,    // 课程id
+				TeacherName: value.Jsxm,   // 教师姓名
+				ClassName:   value.Jxbmc,  // 课程名字
+				Credits:     value.Xf,     // 学分
+				SubmitTime:  value.Tjsj,   // 提交时间
+				SubmitName:  value.Tjrxm,  // 提交名称
+				LessonType:  value.Kcxzmc, // 课程类型
+				ExamType:    value.Ksxz,   // 考试类型
+				SchoolTerm:  value.Xqmmc,  // 学期
+				SchoolYear:  value.Xnmmc,  // 学年
+				Key:         value.Key,    // id
 			})
 	}
 
